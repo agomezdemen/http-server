53f29e9c5a68bd553a4b27b0ed046a4bc481a0ff
agomezdemen <agomezdemen15@gmail.com>
2026-05-27 00:39:23 -0500

    Finshished fd wrapper

--- include/server/net/fd.h
+++ include/server/net/fd.h
@@ -12,12 +12,15 @@ private:
 
 public:
   Fd();                                // empty constructor
-  Fd(int);                             // value constructor
-  Fd(int &&);                          // constructor
-  Fd(const int &) = delete;            // deleting copy constructor
-  Fd &operator=(const int &) = delete; // delting copy assignment
-  Fd &operator=(int &&);               // move assignment
-  ~Fd();                               // constructor
+  explicit Fd(int);                   // value constructor
+  Fd(Fd &&) noexcept;                 // constructor
+  Fd(const Fd &) = delete;            // deleting copy constructor
+  Fd &operator=(const Fd &) = delete; // delting copy assignment
+  Fd &operator=(Fd &&) noexcept;      // move assignment
+  ~Fd() noexcept;                               // constructor
+  bool valid() const noexcept; // checks validity of the Fd object
+  int get() const noexcept; // get sthe current fd handle
+  int release() noexcept; // releases the resource
 };
 
 #endif
--- src/net/fd.cpp
+++ src/net/fd.cpp
@@ -1,1 +1,44 @@
 #include "../../include/server/net/fd.h"
+#include <unistd.h>
+
+Fd::Fd() {
+  fd_ = -1;
+}
+
+Fd::Fd(int input_fd) : fd_{input_fd} {}
+
+Fd::Fd(Fd &&other) noexcept : fd_{other.fd_} {
+  other.fd_ = -1;
+}
+
+auto Fd::operator=(Fd &&other) noexcept -> Fd& {
+  if(this != &other){
+    if(fd != -1)
+      ::close(fd_);
+    fd_ = other.fd_;
+    other.fd_ = -1;
+  }
+
+  return *this;
+}
+
+Fd::~Fd() noexcept {
+  if(fd_ != -1) {
+    ::close(fd_);
+    fd_ = -1;
+  }
+}
+
+
+bool valid() const noexcept {
+  return (fd_ != -1);
+}
+
+int get() const noexcept {
+  return fd_;
+}
+int release() noexcept {
+  int fd_copy = fd_;
+  fd_ = -1;
+  return fd_copy;
+}
